21d5500033cf1da325474d6c7955ac01686bca71
trastephenson <mrtravisstephenson@gmail.com>
2023-04-19 22:28:01 -0600

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -26,8 +26,8 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>mrtravisstephenson@gmail.com</h5>
-            <a href="mailto:mrtravisstephenson@gmail.com" target="_blank">Send a message</a>
+            <h5>traviss.dev@gmail.com</h5>
+            <a href="mailto:traviss.dev@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
             <RiMessengerLine className='contact__option-icon'/>
